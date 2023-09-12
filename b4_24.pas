37ccbb55c237fcfa8cb9da9ef9cb0b7ca9bdb61a
ViktorR-BarreL <144677749+ViktorR-BarreL@users.noreply.github.com>
2023-09-12 19:33:48 +0300

    Add files via upload

---
+++ b4_24.pas
@@ -0,0 +1,10 @@
+﻿program b4_24;
+
+var a,b,x: integer;
+
+begin
+readln (a,b);
+if (a = 0) and (b = 0) then writeln ('INF');
+if (a = 0) and (b <> 0) then writeln ('NO');
+if (a <> 0) then writeln (x:=(-b)/a);
+end.
---
+++ b4_26.pas
@@ -0,0 +1,9 @@
+﻿program b4_26;
+
+var a, b, c: real;
+
+begin
+  writeln ('Введите стороны треугольника');
+  readln (a, b, c);
+  if (a+b > c) and (a+c > b) and (b+c > a) then writeln ('Треугольник существует') else writeln ('Треугольник не существует');
+end.
